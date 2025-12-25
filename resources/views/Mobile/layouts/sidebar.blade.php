87119e9f854a2b40c58e7e2dc1456442e5790b53
Shaqayeq Temori <edristemory33@gmail.com>
2025-12-25 13:53:48 +0430

    new

--- resources/views/Mobile/layouts/sidebar.blade.php
+++ resources/views/Mobile/layouts/sidebar.blade.php
@@ -5,7 +5,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>داشبورد</title>
   @include('Mobile.layouts.links')
-  @vite('resources/css/app.css')
 </head>
 <style>
     .fade-enter {
