0546d90927bfb1fc4b76e94e057a3bbd77dc81b6
sha <edristemory33@gmail.com>
2026-02-12 08:15:59 +0430

    new

--- create_admin.php
+++ create_admin.php
@@ -1,19 +1,12 @@
 <?php
-
 require __DIR__ . '/vendor/autoload.php';
-
 $app = require __DIR__ . '/bootstrap/app.php';
 $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
 use App\Models\UserForm;
 use Illuminate\Support\Facades\Hash;
-
-// اول فقط رکورد را بگیر یا بساز (بدون INSERT)
 $user = UserForm::firstOrNew([
     'username' => 'admin',
 ]);
-
-// بعد تمام فیلدهای اجباری را پر کن
 $user->forceFill([
     'first_name' => 'Admin',
     'last_name'  => 'System',
@@ -24,7 +17,5 @@ $user->forceFill([
     'address'    => 'کابل',
     'number'     => '0700000000',
 ]);
-
 $user->save();
-
 echo "Admin created/updated ✅ | username: admin | password: 12345678\n";
